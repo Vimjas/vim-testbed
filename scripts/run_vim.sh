ff5bf665edd5455674503d524d357d426530ce72
Daniel Hahler <github@thequod.de>
2017-11-04 21:26:37 +0100

    scripts/run_vim.sh: special ARGS handling only with "su" (#49)

--- scripts/run_vim.sh
+++ scripts/run_vim.sh
@@ -17,21 +17,20 @@ fi
 # Set default vimrc to a visible file
 ARGS="-u /home/vimtest/vimrc -i NONE"
 
-# So we can pass the arguments to Vim as it was passed to this script
-while [ $# -gt 0 ]; do
-  ARGS="$ARGS \"$1\""
-  shift
-done
-
 # Run as the vimtest user (when no USER is specified in the Dockerfile, i.e.
 # when running as root).
 # This is not really for security.  It is for running Vim as a user that's
 # unable to write to your volume.
 if [ "$(id -u)" = 0 ]; then
+  # So we can pass the arguments to Vim as it was passed to this script
+  while [ $# -gt 0 ]; do
+    ARGS="$ARGS \"$1\""
+    shift
+  done
   exec su -l vimtest -c "cd /testplugin && /vim-build/bin/$BIN $ARGS"
 fi
 
 cd /testplugin || exit
 
 # shellcheck disable=SC2086
-exec "/vim-build/bin/$BIN" $ARGS
+exec "/vim-build/bin/$BIN" "$@"

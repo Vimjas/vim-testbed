85a9a560b88af51e1629132f727b0f020c8fe1f3
Daniel Hahler <git@thequod.de>
2020-11-16 11:46:51 +0100

    install patch if necessary

--- scripts/install_vim.sh
+++ scripts/install_vim.sh
@@ -216,6 +216,7 @@ build() {
     # Vim patch 8.1.2201 (cannot build with dynamically linked Python 3.8).
     if [ -n "$PYTHON3" ]; then
       if ! grep -q "# if PY_VERSION_HEX >= 0x030800f0" src/if_python3.c; then
+        apk_add_build_dep patch
         curl https://github.com/vim/vim/commit/13a1f3fb0.patch \
           | sed -n -e '/diff --git a\/src\/version.c b\/src\/version.c/,$ d; p' \
           | patch -p1
